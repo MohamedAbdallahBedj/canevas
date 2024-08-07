9b7e3a9e2dd7189356e2c113eb23f1629fe7c38e
MohamedAbdallahBedj <bedjeghamohamed@yahoo.fr>
2024-08-07 11:00:32 +0100

    Log file for pw changing now is not recreated on node restart

--- utils/logger.js
+++ utils/logger.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 
-const output = fs.createWriteStream('./stdout.log');
-const errorOutput = fs.createWriteStream('./stderr.log');
+const output = fs.createWriteStream('./stdout.log', { flags: 'a' });
+const errorOutput = fs.createWriteStream('./stderr.log', { flags: 'a' });
 // custom simple logger
 const logger = new console.Console(output, errorOutput);
 // use it like console
